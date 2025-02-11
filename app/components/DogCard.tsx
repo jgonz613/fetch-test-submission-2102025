fd920bf0d1899787e20cdf8857c6bdf3831fd7fd
jgonz613 <joshuagonz@gmail.com>
2025-02-10 17:24:51 -0800

    removing dark styling

--- app/components/DogCard.tsx
+++ app/components/DogCard.tsx
@@ -8,7 +8,7 @@ interface DogCardType {
 
 const DogCard = ({ dog }: DogCardType) => {
   return (
-    <div className="flex flex-row items-center bg-white border border-gray-200 rounded-lg mb-4 shadow-sm md:flex-row md:max-w-m  h-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <div className="flex flex-row items-center bg-white border border-gray-200 rounded-lg mb-4 shadow-sm md:flex-row md:max-w-m  h-full hover:bg-gray-100 ">
 
       <div className="w-full h-full max-w-[140px] flex items-center justify-center overflow-hidden bg-gray-100">        
         <img
@@ -18,15 +18,15 @@ const DogCard = ({ dog }: DogCardType) => {
       />
       </div>
       <div className="flex flex-col justify-between px-6 ">
-        <h3 className="text-xl font-semibold text-gray-800 dark:text-white">{dog.name}</h3>
+        <h3 className="text-xl font-semibold text-gray-800 ">{dog.name}</h3>
         <div className="space-y-1 min-h-[120px]">
-          <p className="text-sm text-gray-600 dark:text-gray-300">
+          <p className="text-sm text-gray-600 ">
             <strong>Age:</strong> {dog.age}
           </p>
-          <p className="text-sm text-gray-600 dark:text-gray-300">
+          <p className="text-sm text-gray-600 ">
             <strong>Zip:</strong> {dog.zip_code}
           </p>
-          <p className="text-sm text-gray-600 dark:text-gray-300">
+          <p className="text-sm text-gray-600 ">
             <strong>Breed:</strong> {dog.breed}
           </p>
         </div>
